0ecf5afb4ec5a8b21b491bf117de5f2c59351ae3
Tony Wang <yangwang@wisers.com>
2020-09-03 17:12:44 +0800

    [add] common util fetchApi

---
+++ utils/fetchApi.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import responseStatus from 'constants/responseStatus'
+
+export default async (axiosOption) => {
+  const response = await axios(axiosOption)
+
+  if(response.data.status === responseStatus.USER_NOT_LOGIN) {
+    window.location.href = '/login'
+  }
+
+  if(response.data.status !== responseStatus.API_SUCCESS) {
+    throw response.data.status
+  }
+
+  return response.data.data
+}
