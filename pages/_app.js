d366b3ff51978ef0b0cc9cae70a752a5b8c3fb85
Tony Wang <yangwang@wisers.com>
2020-09-03 17:04:33 +0800

    [mod] pages/_app add global style and initialize

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import App from 'next/app'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import withReduxSaga from 'next-redux-saga'
+import { initialize } from 'containers/App/actions'
+import GlobalStyles from 'containers/App/GlobalStyles'
 
 import createStore from '../store'
 
@@ -16,11 +18,20 @@ class MyApp extends App {
     return { pageProps }
   }
 
+  componentDidMount() {
+    if(this.props.router.pathname !== '/login') {
+      this.props.store.dispatch(initialize())
+    }
+  }
+
   render() {
     const { Component, pageProps, store } = this.props
     return (
       <Provider store={store}>
-        <Component {...pageProps} />
+        <>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </>
       </Provider>
     )
   }
