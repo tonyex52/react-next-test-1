a1b2a4c357b9650e87492a00aad730354ca7ffb6
Tony Wang <yangwang@wisers.com>
2020-09-08 11:21:15 +0800

    [mod] add host and port default value

--- server/config.js
+++ server/config.js
@@ -1,8 +1,12 @@
+const host = process.env.host || 'localhost'
+const port = parseInt(process.env.PORT, 10) || 3000
 const redisHost = process.env.redisHost || 'localhost'
 const redisPort = process.env.redisPort || 6379
 const redisPassword = process.env.redisPassword || ''
 
 module.exports = {
+  host,
+  port,
   redisHost,
   redisPort,
   redisPassword,
--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')()
 const Next = require('next')
 
-const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 
 const session = require('express-session')
@@ -121,7 +120,7 @@ app
 
 /* NextJS handler END */
 
-express.listen(port, (err) => {
+express.listen(config.port, (err) => {
   if (err) throw err
-  console.log(`> Ready on http://localhost:${port}`)
+  console.log(`> Ready on http://localhost:${config.port}`)
 })
