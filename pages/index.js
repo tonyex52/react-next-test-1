1dac9a10ac4498ac7bc3d589e6653206cebb4908
Tony Wang <yangwang@wisers.com>
2020-09-08 11:21:50 +0800

    [mod] add PageView container in index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,5 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
+import PageView from 'containers/PageView'
 
-import { loadData, startClock, tickClock } from '../actions'
-import Page from '../components/page'
+const Index = () => <PageView title="首頁" />
 
-class Index extends Component {
-  static async getInitialProps(props) {
-    const { store, isServer } = props.ctx
-    store.dispatch(tickClock(isServer))
-
-    if (!store.getState().placeholderData) {
-      store.dispatch(loadData())
-    }
-
-    return { isServer }
-  }
-
-  componentDidMount() {
-    this.props.dispatch(startClock())
-  }
-
-  render() {
-    return <Page title="Index Page" linkTo="/other" NavigateTo="Other Page" />
-  }
-}
-
-export default connect()(Index)
+export default Index
