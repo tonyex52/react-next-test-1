b5bb86b8f13091d1d24fadf4aed73a87177a782a
Tony Wang <yangwang@wisers.com>
2020-09-08 11:22:00 +0800

    [add] theme

---
+++ constants/theme.js
@@ -0,0 +1,8 @@
+export const primaryColor = '#686fff'
+export const secondaryColor = '#e7ef46'
+
+export const fontGrayColor = 'rgb(0 0 0 / 50%)'
+
+export const separatorColor = '#cacaca'
+
+export const boxShadow = '0px 6px 11px 0px #d4d0d0'
