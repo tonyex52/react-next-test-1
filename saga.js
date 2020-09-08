b8309ddd76529fc5ad47054f9ca065ec1ba2e2fd
Tony Wang <yangwang@wisers.com>
2020-09-08 11:23:00 +0800

    [mod] add PageView saga

--- saga.js
+++ saga.js
@@ -1,13 +1,12 @@
 import { all, fork } from 'redux-saga/effects'
 import es6promise from 'es6-promise'
-import appSaga from './containers/App/saga'
+import appSaga from 'containers/App/saga'
+import pageViewSaga from 'containers/PageView/saga'
 
 es6promise.polyfill()
 
 function* rootSaga() {
-  yield all([
-    fork(appSaga),
-  ])
+  yield all([fork(appSaga), fork(pageViewSaga)])
 }
 
 export default rootSaga
